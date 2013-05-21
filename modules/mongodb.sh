9a30c3c835f5598061dbfc9632594434be7cbf5b
Neo Chan <neo.chan@live.com>
2013-05-21 16:37:15 +0800

    redis.sh

--- modules/mongodb.sh
+++ modules/mongodb.sh
@@ -5,8 +5,25 @@ if [ -z "$( egrep "CentOS|Redhat" /etc/issue)" ]; then
 fi
 
 yum install mongodb-server mongodb -y
+
 chkconfig mongod on
 service mongod start
 
 #yum install mongodb
 
+cat <<EOF | mongo
+use admin
+db.addUser('admin','chen')
+db.system.users.find()
+exit
+EOF
+
+echo "admin password: chen"
+
+cp /etc/mongodb.conf{,.original}
+sed -i "s/#auth = true/auth = true/" /etc/mongodb.conf
+
+service mongod reload
+
+iptables -A INPUT -m state --state NEW -m tcp -p tcp --dport 27017 -j ACCEPT
+service iptables save
