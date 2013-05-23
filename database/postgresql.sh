d6b020598225cb1c10ba24638d062a0ad4ec3cbf
Neo Chan <neo.chan@live.com>
2013-05-23 14:43:26 +0800

    add postgresql.sh

---
+++ database/postgresql.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+#!/bin/bash
+TITLE="PostgreSQL"
+
+PKG=$(whiptail --title "$TITLE" --menu "Options:" 10 50 2 \
+"1" "PostgreSQL server programs" \
+"2" "PostgreSQL client programs" \
+3>&1 1>&2 2>&3)
+
+case "$PKG" in
+	1)
+		yum install postgresql-server
+		;;
+	2)
+		yum install postgresql
+		;;
+	*)
+		
+		RETVAL=1
+esac
+
+exit $RETVAL
