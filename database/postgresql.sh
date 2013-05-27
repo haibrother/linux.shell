76ac816b7fb873919af1372ea534dc0e63c0a035
Neo Chan <neo.chan@live.com>
2013-05-27 15:47:06 +0800

    add new item

--- database/postgresql.sh
+++ database/postgresql.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
-
-#!/bin/bash
 TITLE="PostgreSQL"
 
-PKG=$(whiptail --title "$TITLE" --menu "Options:" 10 50 2 \
+PKG=$(whiptail --title "$TITLE" --menu "Options:" 20 60 5 \
 "1" "PostgreSQL server programs" \
 "2" "PostgreSQL client programs" \
+"3" "PostgreSQL server and client programs" \
+"4" "Pgpool is a connection pooling/replication server" \
 3>&1 1>&2 2>&3)
 
 case "$PKG" in
@@ -14,6 +14,12 @@ case "$PKG" in
 		;;
 	2)
 		yum install postgresql
+		;;
+	3)
+		yum install postgresql-server postgresql
+		;;
+	4)
+		yum install pgpool-II
 		;;
 	*)
 		
