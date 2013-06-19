3d0e70630b922c51596f0cfac7a5c2ada5657cd1
Neo Chan <netkiller@msn.com>
2013-06-19 15:55:56 +0800

    Update ntp.sh

--- modules/ntp.sh
+++ modules/ntp.sh
@@ -6,10 +6,12 @@ if [ -z "$( egrep "CentOS|Redhat" /etc/issue)" ]; then
 fi
 
 yum install ntp -y
-vim /etc/ntp.conf <<VIM > /dev/null 2>&1
-:22,24s/^/#/
-:25,25s/^/\rserver 172.16.3.51\rserver 172.16.3.52\r/
-:wq
-VIM
+
+#vim /etc/ntp.conf <<VIM > /dev/null 2>&1
+#:22,24s/^/#/
+#:25,25s/^/\rserver 172.16.3.51\rserver 172.16.3.52\r/
+#:wq
+#VIM
+
 service ntpd start
 chkconfig ntpd on
