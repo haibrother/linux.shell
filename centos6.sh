c06317f7a486ec7d16adc3b774f66300cb126d88
Neo Chan <netkiller@msn.com>
2013-07-25 15:23:52 +0800

    Update centos6.sh
    net.core.netdev_max_backlog=3000

--- centos6.sh
+++ centos6.sh
@@ -38,7 +38,8 @@ net.ipv4.tcp_syncookies = 1
 net.ipv4.tcp_tw_reuse = 1
 net.ipv4.tcp_tw_recycle = 1
 net.ipv4.tcp_fin_timeout = 60
-net.ipv4.tcp_keepalive_time = 1200
+net.ipv4.tcp_keepalive_time = 1800
+net.core.netdev_max_backlog=3000
 net.ipv4.tcp_max_syn_backlog = 8192
 net.ipv4.tcp_max_tw_buckets = 4096
 EOF
